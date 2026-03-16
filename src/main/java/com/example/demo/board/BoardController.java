f5fa5c270d44aebd89280634d5145b592f1fc7e4
sarapoba <lhk981112@gmail.com>
2026-03-16 14:33:25 +0900

    [Feat] 페이징 처리

--- src/main/java/com/example/demo/board/BoardController.java
+++ src/main/java/com/example/demo/board/BoardController.java
@@ -29,10 +29,12 @@ public class BoardController {
         return ResponseEntity.ok(BaseResponse.success(result));
     }
 
-
+    //  컨트롤러에 내가 보고 싶은 페이지 번호와 한 페이지당 몇 개씩 보고 싶다하다하는 정보를 전달해야 함
     @GetMapping("/list")
-    public ResponseEntity list() {
-        List<BoardDto.ListRes> dto = boardService.list();
+    public ResponseEntity list(
+            @RequestParam(required = true, defaultValue = "0") int page,    //  페이지는 0번으로 수정
+            @RequestParam(required = true, defaultValue = "5")int size) {   //  페이지당 5개 게시글  // 페이지랑 사이즈를 전달받게 수정 dto만들어서 전달해도 된다. 객체지향스럽게
+        BoardDto.PageRes dto = boardService.list(page, size); // 컨트롤러에서 전달받은걸 서비스에 전달
         return ResponseEntity.ok(BaseResponse.success(dto));
     }
 
